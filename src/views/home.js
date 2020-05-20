c733fdaa9d4d75315b186c9db08947301b23af49
Estefan Hu <estefanhu074@gmail.com>
2020-05-20 13:18:01 -0700

    rendering using faker data

--- src/views/home.js
+++ src/views/home.js
@@ -16,16 +16,24 @@ export const Home = ({ navigation }) => {
         renderItem={({ item }) => {
           return (
             <Pitch
-              genre={'Fiction'}
-              author={'Me'}
-              credibility={0}
-              title={item}
-              description={item}
+              genre={item[0]}
+              author={item[1]}
+              credibility={item[2]}
+              title={item[3]}
+              description={item[4]}
             />
           )
         }}
         keyExtractor={(product, idx) => product + idx}
-        data={Array.from(Array(50), () => faker.lorem.words())}
+        data={Array.from(Array(50),
+          () => [
+            faker.lorem.word(),
+            faker.name.firstName(),
+            faker.random.number(),
+            faker.lorem.words(),
+            faker.lorem.sentence(),
+          ]
+        )}
       />
     </Center>
   );
