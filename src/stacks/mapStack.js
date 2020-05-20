65cf0099bde5db791adb3d536edb4470a05a99c3
Estefan Hu <estefanhu074@gmail.com>
2020-05-20 16:26:11 -0700

    set up base map rendering

--- src/stacks/mapStack.js
+++ src/stacks/mapStack.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Map } from '../views/map';
+import { Story } from '../views/story';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +9,7 @@ export const MapStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name='Map' component={Map} />
+      <Stack.Screen name='Story' component={Story} />
     </Stack.Navigator>
   )
 }
--- src/views/map.js
+++ src/views/map.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Center } from '../components/center';
+import MapView from 'react-native-maps';
+import {
+  StyleSheet,
+  View,
+  Dimensions
+} from 'react-native';
 
 export const Map = () => {
   return (
-    <Center>
-      <Text>Map</Text>
-    </Center>
+    <View style={styles.container}>
+      <MapView style={styles.mapStyle} />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
