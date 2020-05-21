20c91d47c0d230196d6ca79f7abdf4139a3019c8
Estefan Hu <estefanhu074@gmail.com>
2020-05-20 23:28:59 -0700

    installed premis

--- src/stacks/libraryStack.js
+++ src/stacks/libraryStack.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Library } from '../views/library';
+import { Story } from '../views/story';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +9,7 @@ export const LibraryStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name='Library' component={Library} />
+      <Stack.Screen name='Story' component={Story} />
     </Stack.Navigator>
   );
 };
--- src/views/library.js
+++ src/views/library.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import {
-  Text
+  FlatList
 } from 'react-native';
-import { Center } from '../components/center.js';
+import { Pitch } from '../components/pitch';
 
-export const Library = () => {
+import faker from 'faker';
+
+export const Library = ({ navigation }) => {
   return (
-    <Center>
-      <Text>Library</Text>
-    </Center>
+    <FlatList
+      style={{ width: '100%', height: '100%' }}
+      renderItem={({ item }) => {
+        return (
+          <Pitch
+            genre={item[0]}
+            author={item[1]}
+            credibility={item[2]}
+            title={item[3]}
+            description={item[4]}
+            navigation={navigation}
+          />
+        )
+      }}
+      keyExtractor={(product, idx) => product + idx}
+      data={Array.from(Array(50),
+        () => [
+          faker.lorem.word(),
+          faker.name.firstName(),
+          faker.random.number(),
+          faker.lorem.words(),
+          faker.lorem.sentence(),
+        ]
+      )}
+    />
   );
 };
