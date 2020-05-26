16974cd09265caa8ee4705b5e274b0c485251f38
Estefan Hu <estefanhu074@gmail.com>
2020-05-26 08:30:22 -0700

    removed import

--- src/stacks/homeStack.js
+++ src/stacks/homeStack.js
@@ -3,23 +3,24 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Home } from '../views/home';
 import { Story } from '../views/story';
 
-import { GenreSetter } from '../components/genreSetter';
-
 const Stack = createStackNavigator();
 
 export const HomeStack = () => {
   return (
-    <Stack.Navigator
-      initialRouteName='Home'
-      screenOptions={{ headerShown: false}}
-    >
+    <Stack.Navigator initialRouteName='Home'>
       <Stack.Screen
         name='Home'
+        options={{
+          header: () => null
+        }}
         component={Home}
         initialParams={{ genre: 'All' }}
       />
       <Stack.Screen
         name='Story'
+        options={{
+          title: 'hello'
+        }}
         component={Story}
       />
     </Stack.Navigator>
