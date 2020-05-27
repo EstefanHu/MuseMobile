277cbdeb5713d02cfbaf92eea26bc8d7fb794cc9
Estefan Hu <estefanhu074@gmail.com>
2020-05-26 21:16:06 -0700

    installed bottomsheet

--- src/components/bottomSheet.js
+++ src/components/bottomSheet.js
@@ -1,23 +1,62 @@
-import React from 'reat';
+import React, { useRef } from 'react';
 import {
   StyleSheet,
   View,
-  ScrollView,
-  SafeAreaView
+  Animated,
+  PanResponder,
+  Dimensions,
 } from 'react-native';
 
 export const BottomSheet = ({ children }) => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
+  const pan = useRef(new Animated.ValueXY()).current;
 
-      </ScrollView>
-    </SafeAreaView>
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          y: pan.y._value
+        });
+      },
+      onPanResponderMove: Animated.event([null, { dy: pan.y }]),
+      onPanResponderRelease: () => {
+        pan.flattenOffset();
+      }
+    })
+  ).current;
+
+  return (
+    <Animated.View
+      style={{
+        transform: [{ translateY: pan.y }],
+        position: 'absolute',
+      }}
+      {...panResponder.panHandlers}
+    >
+      <View style={styles.container}>
+        <View style={styles.grabber}>
+        </View>
+        {children}
+      </View>
+    </Animated.View >
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    width: Dimensions.get('window').width,
+    borderRadius: 10,
+    alignItems: 'center',
+    padding: 5
+  },
+  grabber: {
+    backgroundColor: '#b8b8b8',
+    width: 40,
+    height: 5,
+    marginTop: 10,
+    borderRadius: 10,
   }
 });
--- src/views/launch.js
+++ src/views/launch.js
@@ -9,13 +9,16 @@ import MapView from 'react-native-maps';
 import { LocationContext } from '../providers/locationProvider.js';
 import { CurrentContext } from '../providers/currentProvider.js';
 
+import { BottomSheet } from '../components/bottomSheet.js';
+
 export const Launch = ({ navigation }) => {
   const { longitude, latitude } = useContext(LocationContext);
   const { current } = useContext(CurrentContext);
 
   return (
-    <View style={styles.contianer}>
-      <MapView
+    <View style={styles.container}>
+      <View style={styles.mapContainer}>
+        <MapView
         style={styles.mapStyle}
         showsUserLocation={true}
         initialRegion={{
@@ -26,12 +29,18 @@ export const Launch = ({ navigation }) => {
         }}
       >
       </MapView>
+      </View>
+      <BottomSheet></BottomSheet>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  contianer: {
+  container: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  mapContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
